23512e568dc8bc1ecc6b9ca5273ea9b983af1336
med miss <medmiss07@gmail.com>
2025-11-10 09:46:55 +0100

    Initial commit

---
+++ src/main/java/examlpe/democyber/DemocyberApplication.java
@@ -0,0 +1,21 @@
+package examlpe.democyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemocyberApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemocyberApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Democyber 123!";
+    }
+
+}
